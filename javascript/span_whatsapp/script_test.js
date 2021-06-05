4003b11aa2763dcb5c172caaf8f13dccd345a115
Herson Rezende <herson.academico@gmail.com>
2021-06-05 19:40:33 -0300

    update 05/06/21

---
+++ javascript/span_whatsapp/script_test.js
@@ -0,0 +1,25 @@
+sendSpan(`
+
+Teste, teste, teste
+Teste, teste, teste
+Teste, teste, teste
+Teste, teste, teste
+Teste, teste, teste
+Teste, teste, teste
+Teste, teste, teste
+Teste, teste, teste
+Teste, teste, teste
+Teste, teste, teste
+Teste, teste, teste
+Teste, teste, teste
+Teste, teste, teste
+Teste, teste, teste
+Teste, teste, teste
+Teste, teste, teste
+Teste, teste, teste
+Teste, teste, teste
+Teste, teste, teste
+
+Fim do Teste xD
+
+`);
